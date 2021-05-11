1cc46b4ed88c9fd03fcc3ed65b2a1157b0cd3c47
Thomas Huynh <thomas.huynh@etu.univ-grenoble-alpes.fr>
2021-05-11 13:39:43 +0200

    Added route for getting user's all deals

--- routes/deal.route.js
+++ routes/deal.route.js
@@ -4,7 +4,7 @@ const dealRoute = express.Router();
 
 let dealModel = require('../model/Deal');
 
-// Add Song
+// Add deal
 dealRoute.route('/create-deal').post((req, res, next) => {
     dealModel.create(req.body, (error, data) => {
         if (error) {
@@ -15,7 +15,7 @@ dealRoute.route('/create-deal').post((req, res, next) => {
     })
 });
 
-// Get all songs
+// Get all deals
 dealRoute.route('/').get((req, res) => {
     dealModel.find((error, data) => {
         if (error) {
@@ -38,7 +38,7 @@ dealRoute.route('/search-deal/:search').get((req, res, next) => {
     });
 });
 
-// Get single song
+// Get single deal
 dealRoute.route('/get-deal/:id').get((req, res) => {
     dealModel.findById(req.params.id, (error, data) => {
         if (error) {
@@ -50,7 +50,7 @@ dealRoute.route('/get-deal/:id').get((req, res) => {
 })
 
 
-// Update song
+// Update deal
 dealRoute.route('/update-deal/:id').put((req, res, next) => {
     dealModel.findByIdAndUpdate(req.params.id, {
         $set: req.body
@@ -65,7 +65,7 @@ dealRoute.route('/update-deal/:id').put((req, res, next) => {
     })
 })
 
-// Delete song
+// Delete deal
 dealRoute.route('/delete-deal/:id').delete((req, res, next) => {
     dealModel.findByIdAndRemove(req.params.id, (error, data) => {
         if (error) {
@@ -74,6 +74,17 @@ dealRoute.route('/delete-deal/:id').delete((req, res, next) => {
             res.status(200).json({
                 msg: data
             })
+        }
+    })
+})
+
+// Delete deal
+dealRoute.route('/get-user-deals/:uid').get((req, res) => {
+    dealModel.find({author: req.params.uid}, (error, data) => {
+        if (error) {
+            return next(error)
+        } else {
+            res.json(data)
         }
     })
 })
